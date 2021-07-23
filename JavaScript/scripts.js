75000f67ff8cfff3c3e0a2717ee4fb8bd4362419
Carlos <carlosedufpaiva@poli.ufrj.br>
2021-07-23 13:57:05 -0300

    The following applications were added: All items get border when selected - When items are selected ineach menu, the bottom button gets its layout changed and gets add an Onclick function.

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,3 +1,46 @@
+function FinishOrder() {
+    alert("É pra já!");
+}
+
+
+function ActivateBottomButton() {
+    let BottomButton = document.querySelector(".bottom-bar button");
+
+    BottomButton.classList.add("activated-button");
+    BottomButton.innerHTML = "Fechar pedido";
+    BottomButton.onclick = FinishOrder;
+}
+
+function PickSelectedMainDish () {
+    let MainDishMenu = document.querySelector(".main-dish ul");
+    let SelectedMainDish = MainDishMenu.querySelector(".activated-item");
+    return SelectedMainDish;
+}
+
+function PickSelectedDrink () {
+    let DrinkMenu = document.querySelector(".drink ul");
+    let SelectedDrink = DrinkMenu.querySelector(".activated-item");
+    return SelectedDrink;
+}
+
+
+function PickSelectedDesert () {
+    let DesertMenu = document.querySelector(".desert ul");
+    let SelectedDesert = DesertMenu.querySelector(".activated-item");
+    return SelectedDesert;
+}
+
+function CheckAllMenus () {
+
+    let SelectedMainDish = PickSelectedMainDish ();
+    let SelectedDrink = PickSelectedDrink ();
+    let SelectedDesert = PickSelectedDesert ();
+    
+    if ( SelectedMainDish !== null && SelectedDrink !== null && SelectedDesert !== null) {
+        ActivateBottomButton();
+    }
+}
+
 function DeactivateOtherItems(ThisElement) {
     let ElementParent = ThisElement.parentNode;
     let SelectedChild = ElementParent.querySelector(".activated-item");
@@ -6,9 +49,9 @@ function DeactivateOtherItems(ThisElement) {
     }
 }
 
-
-
 function ActivateItem(ThisElement) {
-    DeactivateOtherItems(ThisElement)
+    DeactivateOtherItems(ThisElement);
     ThisElement.classList.add("activated-item");
+    CheckAllMenus ();
 }
+
