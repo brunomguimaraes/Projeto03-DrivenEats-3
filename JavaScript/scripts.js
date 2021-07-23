9fd0645cfb44fbde8c98c71780eaf84dacd11f9e
Carlos <carlosedufpaiva@poli.ufrj.br>
2021-07-23 11:50:25 -0300

    Item selection included - Other items of the first menu deactivate once another item is activated - To be implemented for all menus

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,7 +1,14 @@
-function UncheckAllitems() {
+function DeactivateOtherItems(ThisElement) {
+    let ElementParent = ThisElement.parentNode;
+    let SelectedChild = ElementParent.querySelector(".activated-item");
+    if (SelectedChild !== null){
+        SelectedChild.classList.remove("activated-item");
+    }
 }
 
 
+
 function ActivateItem(ThisElement) {
+    DeactivateOtherItems(ThisElement)
     ThisElement.classList.add("activated-item");
 }
